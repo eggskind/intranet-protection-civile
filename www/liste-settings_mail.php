e062206238c675e0361adee28c4e41d8ebf4e187
bpasek <bde245@gmail.com>
2016-06-26 22:57:09 +0200

    settings mail
    Suppression d'un double appel rendant le menu inactif

--- www/liste-settings_mail.php
+++ www/liste-settings_mail.php
@@ -35,8 +35,6 @@ else{
 		<meta name="viewport" content="width=device-width, initial-scale=1.0 user-scalable=no">
 	</head>
 	<body>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-<script src="js/bootstrap.min.js" type="text/javascript" charset="utf-8"></script>
 	<?php include('header.php') ?>
 	<div class="container">
 <?php
