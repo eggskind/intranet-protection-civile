2711da38e62b948f6e3087d9dac7657832b711f1
prestataire <jpasqualini75@gmail.com>
2016-06-28 12:01:45 +0200

    feature-logger : [FEATURE] Ajout d'un point d'entrée unique pour connecter le logger sur toute les pages et préparer le terrain pour de futur évolution

---
+++ www/web.php
@@ -0,0 +1,36 @@
+<?php
+
+// On charges les librairies principales (logger, etc...)
+include __DIR__.'/lib/logger.php';
+
+// On active le logger
+lib\logger\logger_register();
+
+// Url demandé par le client
+$uri = $_SERVER['REQUEST_URI'];
+
+// On protège l'include contre les attaques par chemin relatifs
+$uri = str_replace('..', '', $uri);
+
+// Si aucune page n'est demandé alors on appel la page index.php
+if('/' === $uri) {
+  $uri = '/index.php'; 
+}
+
+// On construit le chemin vers le fichier php
+$pathPagePhp = __DIR__.'/'.$uri; 
+
+// Le page php existe
+if (file_exists($pathPagePhp) && is_file($pathPagePhp)) {
+    // Alors on l'affiche
+    include $pathPagePhp;
+}
+else
+{
+    // Dans le cas contraire on affiche une page par défaut
+    // TODO : Prévoir une page 404
+    echo 'La page demandée n\'existe pas. Retour vers <a href="/index.php">accueil</a>.';
+    
+    header('HTTP/1.0 404 Not Found');
+    exit();
+}
